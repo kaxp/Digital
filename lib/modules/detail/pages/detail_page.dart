d89f8d82f6e897229ce46d8ea872ae0982419100
Kapil Sahu <kapilsahu232@gmail.com>
2023-05-11 17:34:45 +0530

    feature: add localization (#5)

--- lib/modules/detail/pages/detail_page.dart
+++ lib/modules/detail/pages/detail_page.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_base_template_1/generated/l10n.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class DetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Detail Page')),
+      appBar: AppBar(
+        title: Text(
+          S.current.detailPage,
+        ),
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () => Modular.to.navigate('/'),
-          child: const Text('Back to Home'),
+          child: Text(
+            S.current.backToHome,
+          ),
         ),
       ),
     );
--- lib/modules/home/pages/home_page.dart
+++ lib/modules/home/pages/home_page.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_base_template_1/generated/l10n.dart';
 import 'package:flutter_base_template_1/modules/app/base_app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
+      appBar: AppBar(
+        title: Text(
+          S.current.homePage,
+        ),
+      ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () => Modular.to.navigate(BaseAppModuleRoutes.detailPage),
-          child: const Text('Navigate to Detail Page'),
+          onPressed: () => Modular.to.navigate(
+            BaseAppModuleRoutes.detailPage,
+          ),
+          child: Text(
+            S.current.navigateToDetailPage,
+          ),
         ),
       ),
     );
