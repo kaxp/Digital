4bbea8ddb605706a2ca5488d9d350937020797d3
kaxp <kapilsahu232@gmail.com>
2023-05-11 17:31:11 +0530

    Block app in portrait mode and add localization delegates

--- lib/modules/app/root_app.dart
+++ lib/modules/app/root_app.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_base_template_1/constants/widget_keys.dart';
+import 'package:flutter_base_template_1/generated/l10n.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class RootApp extends StatelessWidget {
   const RootApp();
 
-  static final RouteObserver<Route> routeObserver = RouteObserver<Route>();
-
   // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return MaterialApp.router(
       title: 'Flutter template',
       theme: ThemeData(
@@ -25,7 +30,10 @@ class RootApp extends StatelessWidget {
         Locale('en', 'US'),
       ],
       localizationsDelegates: const [
-        ...GlobalMaterialLocalizations.delegates,
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
       ],
     );
   }
