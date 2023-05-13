96a81b9436d666f994f0ec3f8eb3319192688efe
kaxp <kapilsahu232@gmail.com>
2023-05-13 21:40:07 +0530

    Add detail module

---
+++ lib/modules/detail/detail_module.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_base_template_1/modules/detail/bloc/detail_bloc.dart';
+import 'package:flutter_base_template_1/modules/detail/pages/detail_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class DetailModule extends Module {
+  @override
+  List<Bind> get binds => [
+        Bind<DetailBloc>((i) {
+          return DetailBloc();
+        }),
+      ];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute(
+          Modular.initialRoute,
+          child: (context, args) => DetailPage(),
+        ),
+      ];
+}
+
+class DetailRoute {
+  static const String moduleRoute = '/detail/';
+}
