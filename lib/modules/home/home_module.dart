3e872bb057c94d3f755ba1fbe9ac5b671d19a580
kaxp <kapilsahu232@gmail.com>
2023-05-14 00:19:34 +0530

    Bind HomeRepo to HomeModule

--- lib/modules/home/home_module.dart
+++ lib/modules/home/home_module.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_base_template_1/modules/home/bloc/home_bloc.dart';
 import 'package:flutter_base_template_1/modules/home/pages/home_page.dart';
+import 'package:flutter_base_template_1/modules/home/repositories/home_repo.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeModule extends Module {
   @override
   List<Bind> get binds => [
+        Bind<HomeRepo>((_) => HomeRepo()),
         Bind<HomeBloc>((i) {
           return HomeBloc();
         }),
