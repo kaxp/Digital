65be0305fc05c0a7907d94984b8b58a2af1b74c3
kaxp <kapilsahu232@gmail.com>
2023-05-14 00:19:07 +0530

    Binding AppDio to base app module

--- lib/modules/app/base_app_module.dart
+++ lib/modules/app/base_app_module.dart
@@ -1,10 +1,21 @@
 import 'package:flutter_base_template_1/modules/detail/detail_module.dart';
 import 'package:flutter_base_template_1/modules/home/home_module.dart';
+import 'package:flutter_base_template_1/networking/constants/network_constants.dart';
+import 'package:flutter_base_template_1/networking/http_client.dart';
+import 'package:flutter_base_template_1/networking/models/app_dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class BaseAppModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind<AppDio>(
+          (i) => AppDio(
+            noAuthDio: defaultHttpClient(
+              clientId: NetworkConstants.clientId,
+            ),
+          ),
+        ),
+      ];
 
   @override
   List<ModularRoute> get routes => [
