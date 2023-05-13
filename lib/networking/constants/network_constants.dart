cab7109ec8fba13df4f08c4afa09e730681a292c
kaxp <kapilsahu232@gmail.com>
2023-05-14 00:16:44 +0530

    Add base url and client id

--- lib/networking/constants/network_constants.dart
+++ lib/networking/constants/network_constants.dart
@@ -4,12 +4,12 @@ import 'package:flutter_base_template_1/generated/l10n.dart';
 class NetworkConstants {
   static String genericError = S.current.somethingWentWrong;
 
-  static String prodBaseUrl = '';
-  static String prodAuthorization = '';
+  static String prodBaseUrl = 'https://api.seatgeek.com/2';
+  static String prodClientId = 'MzM2NDA4MTZ8MTY4Mzk0NzE4MS44OTEwNjYz';
 
-  static String stagingBaseUrl = '';
-  static String stagingAuthorization = '';
+  static String stagingBaseUrl = 'https://api.seatgeek.com/2';
+  static String stagingClientId = 'MzM2NDA4MTZ8MTY4Mzk0NzE4MS44OTEwNjYz';
 
   static final String baseUrl = FlavorConfig.instance!.values.baseUrl;
-  static final String authorization = FlavorConfig.instance!.values.authorization;
+  static final String clientId = FlavorConfig.instance!.values.clientId;
 }
