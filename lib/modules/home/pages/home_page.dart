7622ef4cd67fb99140f9611fbe0569030648e5e6
kaxp <kapilsahu232@gmail.com>
2023-05-14 18:06:03 +0530

    Integrate Bloc widget to view

--- lib/modules/home/pages/home_page.dart
+++ lib/modules/home/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_base_template_1/generated/l10n.dart';
 import 'package:flutter_base_template_1/modules/detail/detail_module.dart';
-import 'package:flutter_base_template_1/modules/home/repositories/home_repo.dart';
-import 'package:flutter_base_template_1/utils/log.dart';
+import 'package:flutter_base_template_1/modules/home/bloc/home_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,45 +11,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final HomeRepo _homeRepo = HomeRepo();
+  final _homeBloc = Modular.get<HomeBloc>();
+  String? message;
 
   @override
   void initState() {
     super.initState();
-    _getEvent();
-  }
-
-  Future<void> _getEvent() async {
-    try {
-      final result = await _homeRepo.getEventByEventId();
-
-      Log.debug('$result');
-    } catch (e) {
-      Log.error('$e');
-      // never swallow unhandled error
-      rethrow;
-    }
+    _homeBloc.getEventByEventId();
   }
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          S.current.homePage,
-        ),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Modular.to.pushNamed(
-              DetailRoute.moduleRoute,
-            );
-          },
-          child: Text(
-            S.current.navigateToDetailPage,
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: _homeBloc,
+      listener: (context, state) {
+        if (state is HomeLoaded) {
+          message = 'Data fetching is successful';
+        } else if (state is HomeError) {
+          message = state.errorMessage;
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              S.current.homePage,
+            ),
           ),
-        ),
-      ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment:  MainAxisAlignment.center,
+              children: [
+                Text(message ?? ''),
+                const SizedBox(
+                  height: 60,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Modular.to.pushNamed(
+                      DetailRoute.moduleRoute,
+                    );
+                  },
+                  child: Text(
+                    S.current.navigateToDetailPage,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
