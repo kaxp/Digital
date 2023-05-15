1b59635d8ed44dd764523e8a82ef1a0e1f6f0883
Kapil Sahu <kapilsahu232@gmail.com>
2023-05-15 10:31:20 +0530

    feature: add app theme data (#16)

---
+++ lib/config/themes/assets/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+mixin AppColors {
+  static const Color redColor = Color(0xffed4264);
+  static const Color primaryColor = Color(0xff2b3248);
+  static const Color backgroundColor = Colors.white;
+  static const Color blackColor = Color(0xff141414);
+  static const Color disabledColor = Colors.grey;
+}
