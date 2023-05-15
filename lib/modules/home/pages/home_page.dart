d60ac28e442ccb8b76c1382db8d382768a21a284
kaxp <kapilsahu232@gmail.com>
2023-05-15 15:49:23 +0530

    Add static UI in home page

--- lib/modules/home/pages/home_page.dart
+++ lib/modules/home/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_base_template_1/components/atoms/typography/header2.dart';
+import 'package:flutter_base_template_1/components/molecules/app_bar/custom_appbar.dart';
+import 'package:flutter_base_template_1/components/organisms/list_views/events_list_view.dart';
+import 'package:flutter_base_template_1/config/themes/assets/app_fonts.dart';
+import 'package:flutter_base_template_1/config/themes/assets/app_images.dart';
+import 'package:flutter_base_template_1/constants/spacing_constants.dart';
 import 'package:flutter_base_template_1/generated/l10n.dart';
-import 'package:flutter_base_template_1/modules/detail/detail_module.dart';
 import 'package:flutter_base_template_1/modules/home/bloc/home_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -32,31 +37,50 @@ class _HomePageState extends State<HomePage> {
       },
       builder: (context, state) {
         return Scaffold(
-          appBar: AppBar(
-            title: Text(
+          appBar: CustomAppBar(
+            titleWidget: Text(
               S.current.homePage,
+              style: const TextStyle(
+                color: Colors.white,
+                fontFamily: AppFonts.ratMedium,
+              ),
             ),
+            isCenter: true,
+            leadingWidget: const Center(
+              child: Image(
+                width: kSpacingXLarge,
+                height: kSpacingXLarge,
+                image: AssetImage(
+                  AppImages.appLogo,
+                ),
+              ),
+            ),
+            actionWidgets: [
+              IconButton(
+                onPressed: () => {},
+                icon: const Icon(
+                  Icons.search,
+                ),
+              )
+            ],
           ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment:  MainAxisAlignment.center,
-              children: [
-                Text(message ?? ''),
-                const SizedBox(
-                  height: 60,
+          body: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(
+                  kSpacingMedium,
+                  kSpacingMedium,
+                  kSpacingMedium,
+                  kSpacingZero,
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    Modular.to.pushNamed(
-                      DetailRoute.moduleRoute,
-                    );
-                  },
-                  child: Text(
-                    S.current.navigateToDetailPage,
-                  ),
+                child: Header2(
+                  title: S.current.myFavouriteEvents,
+                  fontFamily: AppFonts.ratBold,
+                  fontWeight: FontWeight.w700,
                 ),
-              ],
-            ),
+              ),
+              const EventsListView(),
+            ],
           ),
         );
       },
