476b6d0ad095ae0ebbd23c7aa5d4976d21f6a571
kaxp <kapilsahu232@gmail.com>
2023-05-15 21:21:24 +0530

    Integrate search event api

--- lib/modules/search/bloc/search_bloc.dart
+++ lib/modules/search/bloc/search_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_base_template_1/modules/search/models/events_response.dart';
 import 'package:flutter_base_template_1/modules/search/repositories/search_repo.dart';
 import 'package:flutter_base_template_1/utils/dio_error_extension.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -15,16 +16,27 @@ class SearchBloc extends Cubit<SearchState> {
   Future<void> fetchEvents(String? searchString) async {
     if (searchString == null || searchString.length <= 2) {
       emit(
-        SearchLoaded(),
+        SearchLoaded(
+          eventsData: state.eventsData,
+          searchQuery: state.searchQuery,
+        ),
       );
       return;
     }
 
     try {
-      await _searchRepo.fetchEvents(searchString);
-      emit(
-        SearchLoaded(),
-      );
+      final result = await _searchRepo.fetchEvents(searchString);
+
+      if (result.events.isNotEmpty) {
+        emit(
+          SearchLoaded(
+            eventsData: result,
+            searchQuery: searchString,
+          ),
+        );
+      }else{
+        emit(SearchEmpty());
+      }
     } on DioError catch (error) {
       emit(SearchError(errorMessage: error.errorMessage()));
     }
