6a207b95e6e34f742fcfb945d410211c9950dfba
kaxp <kapilsahu232@gmail.com>
2023-05-15 15:23:37 +0530

    Add spacing constants

---
+++ lib/constants/spacing_constants.dart
@@ -0,0 +1,12 @@
+/// Spacings
+///
+/// A collection of spacing constants.
+/// Usage: Layout padding, Layout margin.
+
+const kSpacingXxSmall = 4.0;
+const kSpacingXSmall = 8.0;
+const kSpacingSmall = 12.0;
+const kSpacingMedium = 16.0;
+const kSpacingLarge = 24.0;
+const kSpacingXLarge = 32.0;
+const kSpacingXxLarge = 40.0;
