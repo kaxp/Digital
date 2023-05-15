04d2696ef7def2e898f82510308350d14fc52b13
kaxp <kapilsahu232@gmail.com>
2023-05-15 21:21:46 +0530

    Refactor state

--- lib/modules/search/bloc/search_state.dart
+++ lib/modules/search/bloc/search_state.dart
@@ -1,16 +1,33 @@
 part of 'search_bloc.dart';
 
 abstract class SearchState extends Equatable {
-  const SearchState();
+  const SearchState({
+    this.eventsData,
+    this.searchQuery,
+  });
+
+  final EventsResponse? eventsData;
+  final String? searchQuery;
+
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [eventsData, searchQuery];
 }
 
 class SearchInitial extends SearchState {}
 
 class SearchLoading extends SearchState {}
 
-class SearchLoaded extends SearchState {}
+class SearchEmpty extends SearchState {}
+
+class SearchLoaded extends SearchState {
+  const SearchLoaded({
+    EventsResponse? eventsData,
+    String? searchQuery,
+  }) : super(
+          eventsData: eventsData,
+          searchQuery: searchQuery,
+        );
+}
 
 class SearchError extends SearchState {
   const SearchError({
