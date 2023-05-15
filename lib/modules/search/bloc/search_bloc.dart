f5160f81ab947d5e7e7728d6fad4e88915510beb
kaxp <kapilsahu232@gmail.com>
2023-05-15 23:01:06 +0530

    Add pagination method in bloc

--- lib/modules/search/bloc/search_bloc.dart
+++ lib/modules/search/bloc/search_bloc.dart
@@ -10,33 +10,77 @@ part 'search_state.dart';
 
 class SearchBloc extends Cubit<SearchState> {
   SearchBloc() : super(SearchInitial());
-
   final _searchRepo = Modular.get<SearchRepo>();
 
   Future<void> fetchEvents(String? searchString) async {
     if (searchString == null || searchString.length <= 2) {
       emit(
         SearchLoaded(
-          eventsData: state.eventsData,
-          searchQuery: state.searchQuery,
+          events: state.events,
+          searchQuery: state.searchQuery!,
+          hasReachedEnd: state.hasReachedEnd,
+          page: state.page,
+          totalResultCount: state.totalResultCount,
         ),
       );
       return;
     }
 
     try {
-      final result = await _searchRepo.fetchEvents(searchString);
+      final response = await _searchRepo.fetchEvents(
+        searchString: searchString,
+        page: state.page,
+      );
 
-      if (result.events.isNotEmpty) {
+      if (response.events.isNotEmpty) {
         emit(
           SearchLoaded(
-            eventsData: result,
+            events: response.events,
             searchQuery: searchString,
+            page: response.meta.page,
+            hasReachedEnd: response.meta.total - 10 <=
+                0, // when (total_result_count - 10 <= 0) == true, means we are at last page.
+            totalResultCount: response.meta.total,
           ),
         );
-      }else{
+      } else {
         emit(SearchEmpty());
       }
+    } on DioError catch (error) {
+      emit(SearchError(errorMessage: error.errorMessage()));
+    }
+  }
+
+  Future<void> loadNextPage(String? searchString) async {
+    if (state.hasReachedEnd || state is! SearchLoaded || state is SearchLoadingMore) {
+      return;
+    }
+
+    try {
+      emit(SearchLoadingMore(
+        events: state.events,
+        searchQuery: state.searchQuery!,
+        hasReachedEnd: state.hasReachedEnd,
+        page: state.page,
+        totalResultCount: state.totalResultCount,
+      ));
+
+      final response = await _searchRepo.fetchEvents(
+        searchString: searchString!,
+        page: state.page + 1,
+      );
+
+      emit(
+        SearchLoaded(
+            events: state.events + response.events,
+            searchQuery: searchString,
+            page: response.meta.page,
+            hasReachedEnd:
+                response.meta.total - 10 <= 0, // when (total_result - 10 <= 0) == true, means we are at last page.
+            totalResultCount:
+                state.totalResultCount - 10 // Reduce the total result count by 10 with each pagination API call.
+            ),
+      );
     } on DioError catch (error) {
       emit(SearchError(errorMessage: error.errorMessage()));
     }
