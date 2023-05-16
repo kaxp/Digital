c5c0db8a290bbef1504aa21760a7f502e12428af
kaxp <kapilsahu232@gmail.com>
2023-05-16 16:17:19 +0530

    Integrate detail bloc with view

--- lib/modules/detail/pages/detail_page.dart
+++ lib/modules/detail/pages/detail_page.dart
@@ -9,90 +9,117 @@ import 'package:flutter_base_template_1/config/themes/assets/app_colors.dart';
 import 'package:flutter_base_template_1/config/themes/assets/app_fonts.dart';
 import 'package:flutter_base_template_1/constants/spacing_constants.dart';
 import 'package:flutter_base_template_1/generated/l10n.dart';
+import 'package:flutter_base_template_1/modules/detail/bloc/detail_bloc.dart';
 import 'package:flutter_base_template_1/modules/home/models/events_response.dart';
 import 'package:flutter_base_template_1/utils/helpers.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   const DetailPage({super.key, required this.event});
 
   final Event event;
 
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  final _detailBloc = Modular.get<DetailBloc>();
+
+  @override
+  void initState() {
+    super.initState();
+    _detailBloc.setInitialPageState(widget.event);
+  }
+
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: CustomAppBar(
-        titleWidget: Text(
-          S.current.detailPage,
-          style: const TextStyle(
-            color: AppColors.backgroundColor,
-            fontFamily: AppFonts.ratMedium,
-          ),
-        ),
-        isCenter: true,
-        actionWidgets: [
-          IconButton(
-            onPressed: () => {},
-            icon: const Icon(
-              Icons.favorite_border,
+
+    return BlocBuilder<DetailBloc, DetailState>(
+      bloc: _detailBloc,
+      builder: (context, state) {
+        return Scaffold(
+          appBar: CustomAppBar(
+            titleWidget: Text(
+              S.current.detailPage,
+              style: const TextStyle(
+                color: AppColors.backgroundColor,
+                fontFamily: AppFonts.ratMedium,
+              ),
             ),
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(kSpacingMedium),
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            isCenter: true,
+            actionWidgets: [
+              IconButton(
+                onPressed: () => _detailBloc.onFavouriteTap(widget.event),
+                icon: state is DetailFavourite
+                    ? const Icon(
+                        Icons.favorite,
+                        color: AppColors.redColor,
+                      )
+                    : const Icon(
+                        Icons.favorite_border,
+                      ),
+              )
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(kSpacingMedium),
+            child: Stack(
               children: [
-                Center(
-                  child: EventDetailBanner(
-                    imageUrl: event.performers[0].image,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: EventDetailBanner(
+                        imageUrl: widget.event.performers[0].image,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: kSpacingMedium,
+                    ),
+                    Header1(
+                      title: widget.event.title,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    const SizedBox(
+                      height: kSpacingXxSmall,
+                    ),
+                    Header3(
+                      title: '${widget.event.venue.city}, ${widget.event.venue.state}',
+                      color: AppColors.lightGreyColor,
+                    ),
+                    const SizedBox(
+                      height: kSpacingXxSmall,
+                    ),
+                    Header3(
+                      title: DateFormatter.formattedFullDateAndTimeWithComma(widget.event.datetimeLocal),
+                      color: AppColors.lightGreyColor,
+                    ),
+                  ],
+                ),
+                Positioned(
+                  bottom: 0,
+                  child: SizedBox(
+                    width: screenWidth - 32,
+                    child: DefaultElevatedButton(
+                      title: S.current.bookNow,
+                      onPressed: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          CustomSnackbar(
+                            message: S.current.comingSoon,
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: kSpacingMedium,
-                ),
-                Header1(
-                  title: event.title,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
-                ),
-                const SizedBox(
-                  height: kSpacingXxSmall,
-                ),
-                Header3(
-                  title: '${event.venue.city}, ${event.venue.state}',
-                  color: AppColors.lightGreyColor,
-                ),
-                const SizedBox(
-                  height: kSpacingXxSmall,
-                ),
-                Header3(
-                  title: DateFormatter.formattedFullDateAndTimeWithComma(event.datetimeLocal),
-                  color: AppColors.lightGreyColor,
                 ),
               ],
             ),
-            Positioned(
-              bottom: 0,
-              child: SizedBox(
-                width: screenWidth - 32,
-                child: DefaultElevatedButton(
-                  title: S.current.bookNow,
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      CustomSnackbar(
-                        message: S.current.comingSoon,
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
