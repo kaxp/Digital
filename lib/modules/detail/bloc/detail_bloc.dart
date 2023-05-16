d6568e57d6932f89e28853488c0a7e9a2cdf1e02
kaxp <kapilsahu232@gmail.com>
2023-05-16 16:17:07 +0530

    Add method to change favourite event status in bloc

--- lib/modules/detail/bloc/detail_bloc.dart
+++ lib/modules/detail/bloc/detail_bloc.dart
@@ -1,8 +1,32 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter_base_template_1/modules/home/bloc/home_bloc.dart';
+import 'package:flutter_base_template_1/modules/home/models/events_response.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 part 'detail_state.dart';
 
 class DetailBloc extends Cubit<DetailState> {
   DetailBloc() : super(DetailInitial());
+
+  final _homeBloc = Modular.get<HomeBloc>();
+
+  void setInitialPageState(Event event) {
+    if (event.isFavourite) {
+      emit(DetailFavourite());
+    } else {
+      emit(DetailUnFavourite());
+    }
+  }
+
+  void onFavouriteTap(Event event) {
+    _homeBloc.updateFavouriteEventStatus(event.id);
+
+    // TODO(kaxp): Save this data to SharedPrefs.
+    if (state is DetailUnFavourite) {
+      emit(DetailFavourite());
+    } else {
+      emit(DetailUnFavourite());
+    }
+  }
 }
