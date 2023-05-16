d79ede2fe64333c75a095e45049653297dbc1049
kaxp <kapilsahu232@gmail.com>
2023-05-16 23:44:47 +0530

    Add pagination method test

--- test/modules/home/bloc/home_bloc_test.dart
+++ test/modules/home/bloc/home_bloc_test.dart
@@ -17,7 +17,7 @@ import '../../../mocks/repositories/mock_repositories.mocks.dart';
 import '../../../mocks/services/mock_services.mocks.dart';
 import '../../../test_utils/mock_dio_error.dart';
 
-EventsResponse defaultEventsResponse() {
+EventsResponse _defaultEventsResponse() {
   return buildEventsResponseFromTemplate(
     events: [
       Event(
@@ -33,13 +33,33 @@ EventsResponse defaultEventsResponse() {
           title: 'Some title',
           venue: const Venue(city: 'City 1', state: 'State 1')),
     ],
-    meta: const MetaData(total: 10, page: 1, perPage: 10),
+    meta: const MetaData(total: 30, page: 1, perPage: 10),
   );
 }
 
-EventsResponse defaultEmptyEventsResponse() {
+EventsResponse _defaultEmptyEventsResponse() {
   return buildEventsResponseFromTemplate(
     events: [],
+    meta: const MetaData(total: 30, page: 1, perPage: 10),
+  );
+}
+
+EventsResponse _defaultLastPageEventsResponse() {
+  return buildEventsResponseFromTemplate(
+    events: [
+      Event(
+          id: 1,
+          performers: const [Performer(id: 1, image: '', images: Images(huge: ''))],
+          datetimeLocal: DateTime.now(),
+          title: 'Some title',
+          venue: const Venue(city: 'City 1', state: 'State 1')),
+      Event(
+          id: 1,
+          performers: const [Performer(id: 1, image: '', images: Images(huge: ''))],
+          datetimeLocal: DateTime.now(),
+          title: 'Some title',
+          venue: const Venue(city: 'City 1', state: 'State 1')),
+    ],
     meta: const MetaData(total: 10, page: 1, perPage: 10),
   );
 }
@@ -64,7 +84,7 @@ void main() async {
     _mockSharedPreferencesManager = MockSharedPreferencesManager();
 
     when(_mockHomeRepo.fetchEventsData(searchString: 'Hello', page: 1))
-        .thenAnswer((_) => Future.value(defaultEventsResponse()));
+        .thenAnswer((_) => Future.value(_defaultEventsResponse()));
   });
 
   setUp(() async {
@@ -103,7 +123,7 @@ void main() async {
       final bloc = HomeBloc();
 
       when(_mockHomeRepo.fetchEventsData(searchString: 'Hello', page: 1))
-          .thenAnswer((_) => Future.value(defaultEventsResponse()));
+          .thenAnswer((_) => Future.value(_defaultEventsResponse()));
 
       await bloc.fetchEvents('Hello');
       expect(bloc.state, isA<HomeLoaded>());
@@ -116,7 +136,7 @@ void main() async {
       final bloc = HomeBloc();
 
       when(_mockHomeRepo.fetchEventsData(searchString: 'Hello', page: 1))
-          .thenAnswer((_) => Future.value(defaultEmptyEventsResponse()));
+          .thenAnswer((_) => Future.value(_defaultEmptyEventsResponse()));
 
       await bloc.fetchEvents('Hello');
       expect(bloc.state, isA<HomeEmpty>());
@@ -133,6 +153,93 @@ void main() async {
       );
 
       await bloc.fetchEvents('Hello');
+      expect(bloc.state, isA<HomeError>());
+    });
+  });
+
+  group('fetch pagination data from loadNextPage method', () {
+    test('''Given loadNextPage() is called and state is not HomeLoaded
+        Then pagination should not happen and method should return''', () async {
+      final bloc = HomeBloc();
+
+      when(_mockHomeRepo.fetchEventsData(searchString: 'Hello', page: 1)).thenAnswer(
+        (_) async => throw MockDioError(),
+      );
+
+      await bloc.fetchEvents('Hello');
+      when(bloc.state is HomeError);
+
+      await bloc.loadNextPage('Hello');
+
+      verifyNever(_mockHomeRepo.fetchEventsData(searchString: 'Hello', page: 2));
+    });
+
+    test('''Given loadNextPage() is called and state is HomeLoaded
+        When last page of pagination is already fetched
+        Then pagination should not happen and method should return''', () async {
+      final bloc = HomeBloc();
+
+      when(_mockHomeRepo.fetchEventsData(searchString: 'Hello', page: 1))
+          .thenAnswer((_) => Future.value(_defaultLastPageEventsResponse()));
+      await bloc.fetchEvents('Hello');
+      when(bloc.state is HomeLoaded);
+
+      await bloc.loadNextPage('How are you');
+
+      verifyNever(_mockHomeRepo.fetchEventsData(searchString: 'How are you', page: 2));
+    });
+
+    test('''Given loadNextPage() is called and state is HomeLoaded
+         When value of searchString is "How are you" and page is 2
+        And fetchEventsData method during pagination return the response
+        Then state should change to HomeLoaded''', () async {
+      final bloc = HomeBloc();
+
+      when(_mockHomeRepo.fetchEventsData(searchString: 'Hello', page: 1))
+          .thenAnswer((_) => Future.value(_defaultEventsResponse()));
+      await bloc.fetchEvents('Hello');
+      when(bloc.state is HomeLoaded);
+
+      when(_mockHomeRepo.fetchEventsData(searchString: 'How are you', page: 2))
+          .thenAnswer((_) => Future.value(_defaultEventsResponse()));
+      await bloc.loadNextPage('How are you');
+      expect(bloc.state, isA<HomeLoaded>());
+    });
+
+    test('''Given loadNextPage() is called and state is HomeLoaded
+        When value of searchString is "How are you" and page is 2
+        And fetchEventsData method during pagination return empty event list
+        Then state should stay in HomeLoaded''', () async {
+      final bloc = HomeBloc();
+
+      when(_mockHomeRepo.fetchEventsData(searchString: 'Hello', page: 1))
+          .thenAnswer((_) => Future.value(_defaultEventsResponse()));
+      await bloc.fetchEvents('Hello');
+      when(bloc.state is HomeLoaded);
+
+      when(_mockHomeRepo.fetchEventsData(searchString: 'How are you', page: 2))
+          .thenAnswer((_) => Future.value(_defaultEmptyEventsResponse()));
+      await bloc.loadNextPage('How are you');
+
+      expect(bloc.state, isA<HomeLoaded>());
+    });
+
+    test('''Given loadNextPage() is called and state is HomeLoaded
+        When value of searchString is "How are you" and page is 2
+        And fetchEventsData method during pagination return DioError
+        Then state should change to HomeError''', () async {
+      final bloc = HomeBloc();
+
+      when(_mockHomeRepo.fetchEventsData(searchString: 'Hello', page: 1))
+          .thenAnswer((_) => Future.value(_defaultEventsResponse()));
+      await bloc.fetchEvents('Hello');
+      when(bloc.state is HomeLoaded);
+
+      when(_mockHomeRepo.fetchEventsData(searchString: 'How are you', page: 2)).thenAnswer(
+        (_) async => throw MockDioError(),
+      );
+      await bloc.loadNextPage('How are you');
+
       expect(bloc.state, isA<HomeError>());
     });
   });
