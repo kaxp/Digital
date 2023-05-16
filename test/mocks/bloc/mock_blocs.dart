9afd2b895effb6ef0100598cef277bbbf4b41cd6
kaxp <kapilsahu232@gmail.com>
2023-05-17 01:39:41 +0530

    Add DetailBloc to mock

--- test/mocks/bloc/mock_blocs.dart
+++ test/mocks/bloc/mock_blocs.dart
@@ -1,8 +1,10 @@
+import 'package:flutter_base_template_1/modules/detail/bloc/detail_bloc.dart';
 import 'package:flutter_base_template_1/modules/home/bloc/home_bloc.dart';
 import 'package:mockito/annotations.dart';
 
 @GenerateMocks([
-  HomeBloc
+  HomeBloc,
+  DetailBloc,
 ], customMocks: [
 ])
 void main() {}
