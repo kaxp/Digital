34ca15f16864c2c3cf588b49041ac337ef4cdf18
kaxp <kapilsahu232@gmail.com>
2024-01-17 22:38:31 +0530

    Refactor class extention

--- lib/modules/home/home_module.dart
+++ lib/modules/home/home_module.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_base_template_1/managers/shared_preferences_manager.dart';
+import 'package:flutter_base_template_1/modules/app/base_app_module.dart';
 import 'package:flutter_base_template_1/modules/home/bloc/home_bloc.dart';
 import 'package:flutter_base_template_1/modules/home/pages/home_page.dart';
 import 'package:flutter_base_template_1/modules/home/repositories/home_repo.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-class HomeModule extends Module {
+class HomeModule extends BaseAppModule {
   @override
   List<Bind> get binds => [
         Bind<HomeRepo>((_) => HomeRepo()),
